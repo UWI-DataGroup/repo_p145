460a2e029f52184ff0b12cc159997e7d54716269
Kern Rocke <kernrocke@hotmail.com>
2020-01-12 19:54:52 -0400

    Update to algorithm file

--- Grenada_Walk/Gren_Walk_001.py
+++ Grenada_Walk/Gren_Walk_001.py
@@ -8,7 +8,10 @@
 ##	Date Modified: 	12/01/2019
 ##  Algorithm Task: Importing Grenada Shapefiles
 
-# MAC
+
+# Note the below syntax pertains to applications to QGIS python counsel. 
+
+# MAC path
 
 ED = "/Volumes/Secomba/kernrocke/Boxcryptor/DataGroup - repo_data/data_p145/version01/1-input/GIS Data/Grenada_GIS/demographic_data"
 iface.addVectorLayer(ED, "Enumeration District", "ogr")
