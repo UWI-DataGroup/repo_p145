9e8342237c1525f47bbf58ff15c4b362b56491e1
Kern Rocke <kernrocke@hotmail.com>
2020-01-14 09:15:47 -0400

    Update to Gren_Walk_001 algorithm

--- Grenada_Walk/Gren_Walk_001.py
+++ Grenada_Walk/Gren_Walk_001.py
@@ -1,13 +1,20 @@
 
-##  META-DATA
+##  META-DATA COMMENTS
 ##  Program:		Gren_Walk_001
 ##  Project:      	Macroscale Walkability- PhD
 ##	Sub-Project:	Walkability Computation - Grenada 
 ##  Analyst:		Kern Rocke
 ##	Date Created:	12/01/2019
-##	Date Modified: 	13/01/2019
+##	Date Modified: 	14/01/2019
 ##  Algorithm Task: Importing Grenada Shapefiles
 
+
+###########
+
+# Analysis comments
+
+#   This algorithm seeks to import Grenada shapefile into either ArcGIS/QGIS. The layers of interest are:
+#   buildings (land use), road network, enumeration districts and population/residential densityu
 
 # Note the below syntax pertains to applications to ArcGIS python counsel
 
@@ -64,3 +71,9 @@ print ("Script Completed")
 
 ED = "/Volumes/Secomba/kernrocke/Boxcryptor/DataGroup - repo_data/data_p145/version01/1-input/GIS Data/Grenada_GIS/demographic_data"
 iface.addVectorLayer(ED, "Enumeration District", "ogr")
+
+Road = "/Volumes/Secomba/kernrocke/Boxcryptor/DataGroup - repo_data/data_p145/version01/1-input/GIS Data/Grenada_GIS/roads2"
+iface.addVectorLayer(Road, "Road", "ogr")
+
+Building = "/Volumes/Secomba/kernrocke/Boxcryptor/DataGroup - repo_data/data_p145/version01/1-input/GIS Data/Grenada_GIS/buildings_new"
+iface.addVectorLayer(Building, "Building_land_use", "ogr") 
